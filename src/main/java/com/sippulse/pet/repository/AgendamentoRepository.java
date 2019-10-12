a9534b9fb8f3ceb59d4badbe3f44feae41e68e13
Max Guntzel <maxguntzel@gmail.com>
2019-10-12 20:32:07 -0300

    Configuracao hobernate

--- src/main/java/com/sippulse/pet/repository/AgendamentoRepository.java
+++ src/main/java/com/sippulse/pet/repository/AgendamentoRepository.java
@@ -3,7 +3,6 @@ package com.sippulse.pet.repository;
 import com.sippulse.pet.entity.Agendamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.*;
 
 
 /**
@@ -12,5 +11,6 @@ import org.springframework.data.jpa.repository.*;
 @SuppressWarnings("unused")
 @Repository
 public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> {
+
 
 }
